2bf568976c91cf95b3f0d5002c23c11810be92d1
Oleksii S. Malakhov <brezerk@gmail.com>
2018-05-17 14:05:01 +0300

    Update docs

--- docs/html/search/all_0.js
+++ docs/html/search/all_0.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['getnodeguid',['getNodeGUID',['../classibf__node.html#afe265cfd9c0423988b063c0356058a99',1,'ibf_node']]]
+  ['getlocalport',['getLocalPort',['../classibf__node.html#a1edc2663a50b193be7b9a615cc288837',1,'ibf_node']]],
+  ['getlocalportguid',['getLocalPortGUID',['../classibf__node.html#a286ee4769949a09e9bcbdbc88d9a820c',1,'ibf_node']]],
+  ['getnodeguid',['getNodeGUID',['../classibf__node.html#afe265cfd9c0423988b063c0356058a99',1,'ibf_node']]],
+  ['getports',['getPorts',['../classibf__node.html#a82c225df0e9a802f8b146c1a7c069bdb',1,'ibf_node']]]
 ];
